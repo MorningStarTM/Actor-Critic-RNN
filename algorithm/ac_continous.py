9548b254d281d673868942c499fae44044048489
ErnestBeckham <ernestbeckham1998@gmail.com>
2024-07-30 15:29:01 +0530

    agent class created

--- algorithm/ac_continous.py
+++ algorithm/ac_continous.py
@@ -26,3 +26,22 @@ class RNN(nn.Module):
         return torch.zeros(1, self.hidden_size)
     
 
+
+
+class Agent(object):
+    def __init__(self, input_size, actor_lr, critic_lr, gamma=0.99, n_actions=2, layer1_size=64, layer2_size=64, n_outputs=2):
+        self.gamma = gamma
+        self.log_probs = None
+        self.advantages = None
+        self.estimate = None
+        self.ground_truth = None
+        self.entropy = None
+        self.entropy_beta = 0.05
+        self.actor = RNN(input_size, hidden_size=300, output_size=n_outputs, lr=actor_lr)
+        self.critic = RNN(input_size, hidden_size=300, output_size=1, lr=actor_lr)
+        self.actor_hidden = None
+        self.critic_hidden = None
+
+
+    
+    
